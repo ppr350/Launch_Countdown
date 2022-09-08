5e4b5633c6b118c19659a7aa5a5906eecea75dec
ppr350 <kobito610@gmail.com>
2022-09-08 13:45:29 +0100

    Add launch_countdown.js

--- launch_countdown.js
+++ launch_countdown.js
@@ -2,22 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const output = document.querySelector('.output');
 
     function launch() {
-        /*console.log("testing")
-        const para = document.createElement("p");
-                para.textContent = "Blast off!";
-                output.appendChild(para)
-                console.log(para)*/
+
         let i = 10;
 
         do {
             if (i === 10) {
-                /*console.log(i);*/
                 const para = document.createElement("p");
                 output.appendChild(para);
                 para.textContent += "Countdown ";
 
             } if (i < 1){
-                /*console.log(i);*/
                 const para = document.createElement("p");
                 output.appendChild(para);
                 para.textContent += "Blast off";
